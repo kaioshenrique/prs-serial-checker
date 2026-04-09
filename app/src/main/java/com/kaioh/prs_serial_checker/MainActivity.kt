a5aa1c003b83d9760b901ff2e9f6a001e433d177
Kaio Henrique <kaioshenricke@gmail.com>
2026-04-09 00:27:01 -0300

    🚀 Initial commit
    # Conflicts:
    #	.gitignore

---
+++ app/src/main/java/com/kaioh/prs_serial_checker/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kaioh.prs_serial_checker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kaioh.prs_serial_checker.ui.theme.PrsserialcheckerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrsserialcheckerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrsserialcheckerTheme {
+        Greeting("Android")
+    }
+}
